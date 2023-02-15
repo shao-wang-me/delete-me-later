0319a4ba3ffab15c7598d316dd80bd2dd3e99f97
Shao <10136589+shao-wang-me@users.noreply.github.com>
2023-02-16 10:14:47 +1100

    Update small1.js

--- small1.js
+++ small1.js
@@ -2,5 +2,5 @@ builder.OpenFile("https://github.com/shao-wang-me/delete-me-later/raw/main/Small
 var oDocument = Api.GetDocument();
 oDocument.AcceptAllRevisionChanges();
 oDocument.SetTrackRevisions(false);
-builder.SaveFile("docx", "LargeFile.docx");
+builder.SaveFile("docx", "SmallFile1.docx");
 builder.CloseFile();
